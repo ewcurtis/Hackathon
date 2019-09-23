7340b748e36ff1b2cc21d79f59df16df8d540e08
benmoli <peter.malakawi@tcs.com>
2019-09-23 16:46:03 -0400

    Basic layout for Status page

--- Hackathon/src/app/app.module.ts
+++ Hackathon/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StatusComponent } from './status/status.component';
+import { CardApplicationComponent } from './card-application/card-application.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusComponent
+    StatusComponent,
+    CardApplicationComponent
   ],
   imports: [
     BrowserModule
