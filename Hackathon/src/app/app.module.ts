e83d7b95d6ca362052fb24b9139d1075b5ccb681
schmittyy <r.southard@tcs.com>
2019-09-23 12:15:09 -0700

    updating app module

--- Hackathon/src/app/app.module.ts
+++ Hackathon/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardApplicationComponent } from './card-application/card-application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardApplicationComponent
   ],
   imports: [
     BrowserModule
