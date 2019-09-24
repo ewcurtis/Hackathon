6f3cc781f8144166f646abd83959301ef4f5e7ac
jacob.huiet <huietj1@nku.edu>
2019-09-24 09:18:25 -0400

    initial commit

--- Hackathon/src/app/app.module.ts
+++ Hackathon/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgForms } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StatusComponent } from './status/status.component';
 import { CardApplicationComponent } from './card-application/card-application.component';
 
 import { EmployeeComponent } from './employee/employee.component';
-import { CardApplicationComponent } from './card-application/card-application.component';
 import { AppRoutingModule } from './app-routing.module';
 
 
