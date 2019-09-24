6141f6247a08c9c1cbcbbe6e6af9a0974428e8f1
schmittyy <r.southard@tcs.com>
2019-09-24 11:50:29 -0700

    Added new component for a user form

--- Hackathon/src/app/app.module.ts
+++ Hackathon/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgForms } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { CardApplicationComponent } from './card-application/card-application.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CardApplication2Component } from './card-application2/card-application2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    CardApplicationComponent
+    CardApplicationComponent,
+    CardApplication2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
