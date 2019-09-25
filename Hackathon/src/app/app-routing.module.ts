1a3b29551bc2eb953d04735ae1d4b3f62c7443b7
schmittyy <r.southard@tcs.com>
2019-09-25 10:10:04 -0700

    changed homepage and routing for nav bar

--- Hackathon/src/app/app-routing.module.ts
+++ Hackathon/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CardApplicationComponent } from './card-application/card-application.component';
 import { CardApplication2Component } from './card-application2/card-application2.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { StatusComponent } from './status/status.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: CardApplicationComponent },
   { path: 'userForm', component: CardApplication2Component },
-  { path: 'actOnApplication', component: EmployeeComponent }
+  { path: 'actOnApplication', component: EmployeeComponent },
+  { path: 'status', component: StatusComponent }
 ];
 
 @NgModule({
